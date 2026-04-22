f50e7c4d94ef4a7767864397c650ba04839a0dc0
lybahoang <157574648+lybahoang@users.noreply.github.com>
2026-04-23 06:16:40 +0700

    create a template files for Lexer implementation

--- Lexer.h
+++ Lexer.h
@@ -1,1 +1,5 @@
-// fajsfsalk
+/** 
+    The header file for Lexer, which may include an enumeration for
+    the types of tokens, a class for Lexer, and a struct/class for
+    Token (if using class, then it shall provide accessor methods).
+*/
